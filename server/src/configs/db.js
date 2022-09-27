f3a485d17d93a7a41a8edbeb3892326363e0d9bd
Masha Ryzhkova <masharyzhkova30@gmail.com>
2022-09-27 18:35:42 +0300

    make it better

--- server/src/configs/db.js
+++ server/src/configs/db.js
@@ -6,6 +6,14 @@ export const sequelize = new Sequelize(
   process.env.DB_PASSWORD || process.env.DB_PASSWORD_PROD,
   {
     dialect: 'postgres',
-    host: process.env.DB_HOST || process.env.DB_HOST_PROD
+    host: process.env.DB_HOST || process.env.DB_HOST_PROD,
+    ...(process.env.DATABASE_URL && {
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        }
+      }
+    })
   }
 );
